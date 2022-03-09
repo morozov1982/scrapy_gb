b757377ec9b43f1fe7a50561d09be3d7d2eceec1
morozov1982 <morozov1982@mail.com>
2022-03-09 11:41:40 +0600

    add: lesson_07_cean don't work, captcha

--- cian/runner.py
+++ cian/runner.py
@@ -1,16 +1,14 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
-from olxparser import settings
-from olxparser.spiders.olx import OlxSpider
-
+from cian.spiders.cianru import CianruSpider
+from cian import settings
 
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
 
     process = CrawlerProcess(settings=crawler_settings)
-    search = 'котята'  # input('Что ищем?\n>>> ')
-    process.crawl(OlxSpider, search=search)
+    process.crawl(CianruSpider)
 
     process.start()
--- cian/settings.py
+++ cian/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for olxparser project
+# Scrapy settings for cian project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,36 +7,31 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'olxparser'
+BOT_NAME = 'cian'
 
-SPIDER_MODULES = ['olxparser.spiders']
-NEWSPIDER_MODULE = 'olxparser.spiders'
+SPIDER_MODULES = ['cian.spiders']
+NEWSPIDER_MODULE = 'cian.spiders'
 
-IMAGES_STORE = 'photos'
-
-LOG_ENABLED = True
-LOG_LEVEL = 'DEBUG'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
-             '(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+#USER_AGENT = 'cian (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1.5
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = True
+#COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -50,13 +45,13 @@ COOKIES_ENABLED = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'olxparser.middlewares.OlxparserSpiderMiddleware': 543,
+#    'cian.middlewares.CianSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'olxparser.middlewares.OlxparserDownloaderMiddleware': 543,
+#    'cian.middlewares.CianDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -68,8 +63,7 @@ COOKIES_ENABLED = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'olxparser.pipelines.OlxparserPipeline': 300,
-   'olxparser.pipelines.OlxPhotosPipeline': 200,
+   'cian.pipelines.CianPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
